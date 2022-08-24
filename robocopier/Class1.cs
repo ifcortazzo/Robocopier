e4f6620128f3599cceaa2a905d26e26c809c25fc
ifcortazzo <104604929+ifcortazzo@users.noreply.github.com>
2022-08-24 13:48:36 -0300

    Add files via upload

---
+++ robocopier/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System.Management.Automation;
+
+
+namespace robocopier
+{
+    
+    
+    public static class PowerShellHandler
+    {
+
+        private static PowerShell ps = PowerShell.Create();
+
+
+        //Powershell.Create("get-process").Invoke();
+        
+
+
+
+
+    }
+}
---
+++ robocopier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace robocopier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
